fd96fa7277cd62fd13c00844c9efd429ceed9a9c
mosquito-horse245 <159791127+mosquito-horse245@users.noreply.github.com>
2024-09-16 13:44:19 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+for (int i = 1; i <= 100; i++)
+{ 
+Console.WriteLine(7 * i);
+
+}
+Console.WriteLine("Greyson stinks");
